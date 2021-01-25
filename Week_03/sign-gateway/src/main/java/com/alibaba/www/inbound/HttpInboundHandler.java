c40210f729b5c294ea29a28b96ea1fb799af278c
“WYUSIG <13169103974@163.com>
2021-01-25 18:05:27 +0800

    update

--- Week_03/sign-gateway/src/main/java/com/alibaba/www/inbound/HttpInboundHandler.java
+++ Week_03/sign-gateway/src/main/java/com/alibaba/www/inbound/HttpInboundHandler.java
@@ -1,6 +1,13 @@
 package com.alibaba.www.inbound;
 
+import com.alibaba.www.filter.HeaderHttpRequestFilter;
+import com.alibaba.www.filter.HttpRequestFilter;
+import com.alibaba.www.outbound.HttpOutboundHandler;
+import com.alibaba.www.outbound.httpclient4.HttpClientHttpOutboundHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.ReferenceCountUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,4 +18,29 @@ public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
 
     private static Logger logger = LoggerFactory.getLogger(HttpInboundHandler.class);
     private final List<String> proxyServer;
-    private
+    private HttpOutboundHandler handler;
+    private HttpRequestFilter filter = new HeaderHttpRequestFilter();
+
+    public HttpInboundHandler(List<String> proxyServer,HttpOutboundHandler handler){
+        this.proxyServer = proxyServer;
+        this.handler = handler;
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx){
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx,Object msg){
+        try {
+            FullHttpRequest fullHttpRequest = (FullHttpRequest)msg;
+            handler.handle(fullHttpRequest,ctx,filter);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+}
+
--- Week_03/sign-gateway/src/main/java/com/alibaba/www/outbound/httpclient4/HttpClientHttpOutboundHandler.java
+++ Week_03/sign-gateway/src/main/java/com/alibaba/www/outbound/httpclient4/HttpClientHttpOutboundHandler.java
@@ -4,25 +4,52 @@ import com.alibaba.www.filter.HeaderHttpResponseFilter;
 import com.alibaba.www.filter.HttpRequestFilter;
 import com.alibaba.www.filter.HttpResponseFilter;
 import com.alibaba.www.outbound.HttpOutboundHandler;
+import com.alibaba.www.router.HttpEndpointRouter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class HttpClientHttpOutboundHandler implements HttpOutboundHandler {
 
     private CloseableHttpAsyncClient httpClient;
-    private ExecutorService proxyService;
     private List<String> backendUrls;
 
     HttpResponseFilter filter;
+    HttpEndpointRouter router;
 
-
+    public HttpClientHttpOutboundHandler(HttpResponseFilter filter, HttpEndpointRouter router, List<String> backends) {
+        this.filter = filter;
+        this.router = router;
+        this.backendUrls = backends.stream().map(this::formatUrl).collect(Collectors.toList());
+        int cores = Runtime.getRuntime().availableProcessors();
+        IOReactorConfig ioReactorConfig = IOReactorConfig.custom()
+                .setConnectTimeout(1000)
+                .setSoTimeout(1000)
+                .setIoThreadCount(cores)
+                .setRcvBufSize(32*1024)
+                .build();
+        httpClient = HttpAsyncClients.custom().setMaxConnTotal(40)
+                .setMaxConnPerRoute(8)
+                .setDefaultIOReactorConfig(ioReactorConfig)
+                .setKeepAliveStrategy((response,context)->6000)
+                .build();
+        httpClient.start();
+    }
 
     @Override
     public void handle(FullHttpRequest fullHttpRequest, ChannelHandlerContext ctx, HttpRequestFilter filter) {
 
     }
+
+    private String formatUrl(String backend){
+        return backend.endsWith("/")?backend.substring(0,backend.length()-1):backend;
+    }
+
+
 }
